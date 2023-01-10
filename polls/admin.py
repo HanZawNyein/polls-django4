c9926911c2eadb797753597cf95cf72926897dac
HanZawNyein <hanzawnyineonline@gmail.com>
2023-01-10 19:05:27 +0630

    customize inline form and stack inline

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,22 @@ from django.contrib import admin
 from .models import Question,Choice
 
 # Register your models here.
-admin.site.register(Question)
-admin.site.register(Choice)
+# admin.site.register(Question)
+# admin.site.register(Choice)
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+class QuestionAdmin(admin.ModelAdmin):
+    # fields=['pub_date','question_text']
+    fieldsets = [
+        (None,{'fields':['question_text']}),
+        ('Date Information',{'fields':['pub_date']})
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question,QuestionAdmin)
+
+
+
+
