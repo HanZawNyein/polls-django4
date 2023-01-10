32e7a43c84ceeae3e4e2043d7534231e1ad63b07
HanZawNyein <hanzawnyineonline@gmail.com>
2023-01-10 19:06:32 +0630

    tablur inline

--- polls/admin.py
+++ polls/admin.py
@@ -1,23 +1,27 @@
 from django.contrib import admin
-from .models import Question,Choice
+from .models import Question, Choice
 
 # Register your models here.
 # admin.site.register(Question)
 # admin.site.register(Choice)
 
-class ChoiceInline(admin.StackedInline):
+
+# class ChoiceInline(admin.StackedInline):
+#     model = Choice
+#     extra = 3
+
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
+
+
 class QuestionAdmin(admin.ModelAdmin):
     # fields=['pub_date','question_text']
     fieldsets = [
-        (None,{'fields':['question_text']}),
-        ('Date Information',{'fields':['pub_date']})
+        (None, {'fields': ['question_text']}),
+        ('Date Information', {'fields': ['pub_date']})
     ]
     inlines = [ChoiceInline]
 
-admin.site.register(Question,QuestionAdmin)
 
-
-
-
+admin.site.register(Question, QuestionAdmin)
